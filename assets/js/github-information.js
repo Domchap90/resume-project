4a2c4b38b81849d1763ccb0f418b4572af86f3f1
Domchap90 <domchaple@gmail.com>
2020-04-19 15:56:44 +0000

    Rendering Repos completed. Issue with Cross Referencing Origins.

--- assets/js/github-information.js
+++ assets/js/github-information.js
@@ -1,4 +1,4 @@
-function userInformationHTML(user) {
+function userInformationHTML(user) {    // displays information that is called from api int html.
     return `
     <h2>${user.name}
         <span class="small-name">
@@ -15,7 +15,30 @@ function userInformationHTML(user) {
     </div>`;
 }
 
-function fetchGitHubInformation(event) {
+function repoInformationHTML(repos) {
+    console.log("repo function is called");
+    if (repos.length == 0) {
+        return `<div class="clearfix repo-list">No repos!</div>`;
+    }
+    var listItemsHTML = repos.map(function(repo) {
+        console.log("repos.map is called");
+        return `
+        <li>
+            <a href="${repo.html_url}" target="_blank">${repo.name}</a>
+        </li>`;
+    });
+
+    return `<div class="clearfix repo-list">
+        <p>
+            <strong>Repo List:</strong>
+        </p>
+        <ul>
+            ${listItemsHTML.join("\n")}
+        </ul>
+    </div>`;
+}
+
+function fetchGitHubInformation(event) {    // retrieves information from api and stores into variables.
 
     var username = $("#gh-username").val();
     if (!username) {
@@ -29,12 +52,16 @@ function fetchGitHubInformation(event) {
     );
 
     $.when(
-        $.getJSON(`https://api.github.com/users/${username}`)
+        $.getJSON(`https://api.github.com/users/${username}`),
+        $.getJSON(`https://api.github.com/users/${username}/repos`)
     ).then(
-        function (response) {
-            var userData = response;
-            $("#gh-user-data").html(userInformationHTML(userData)); //this function yet to be written
-        }, function (errorResponse) {
+        function (firstResponse, secondResponse) {
+            var userData = firstResponse[0];
+            var repoData = secondResponse[0];
+            $("#gh-user-data").html(userInformationHTML(userData)); 
+            $("#gh-repo-data").html(repoInformationHTML(repoData));
+        }, 
+        function (errorResponse) {
             if (errorResponse.status === 404) {
                 $("#gh-user-data").html(`<h2>No info found for user ${username}</h2>`);
             } else {
@@ -43,4 +70,36 @@ function fetchGitHubInformation(event) {
                     `<h2>Error: ${errorResponse.responseJSON.message}</h2>`);
             }
         });
+}
+
+
+
+
+function createCORSRequest(method, url) {
+  var xhr = new XMLHttpRequest();
+  if ("withCredentials" in xhr) {
+
+    // Check if the XMLHttpRequest object has a "withCredentials" property.
+    // "withCredentials" only exists on XMLHTTPRequest2 objects.
+    xhr.open(method, url, true);
+
+  } else if (typeof XDomainRequest != "undefined") {
+
+    // Otherwise, check if XDomainRequest.
+    // XDomainRequest only exists in IE, and is IE's way of making CORS requests.
+    xhr = new XDomainRequest();
+    xhr.open(method, url);
+
+  } else {
+
+    // Otherwise, CORS is not supported by the browser.
+    xhr = null;
+
+  }
+  return xhr;
+}
+
+var xhr = createCORSRequest('GET', url);
+if (!xhr) {
+  throw new Error('CORS not supported');
 }
